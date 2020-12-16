b4268066da3d75df3b8189aa141ca94885f12521
gladyscodes <glad.alvarez1@gmail.com>
2020-12-15 21:15:45 -0600

    declare, initialize and print variables

--- myhelloworld/Program.cs
+++ myhelloworld/Program.cs
@@ -12,10 +12,19 @@ namespace myhelloworld
         {
             Console.WriteLine("Hello World!");
 
-            // Sinlge line comment test
+            // Single line comment test
             /* Multi line 
              * comment
              * block */
+
+            // VARIABLES 
+            bool test = true;
+            int test1 = 12345;
+            double test2 = 3.5423;
+            char test3 = 'g';
+            string test4 = "Gladys";
+
+            Console.WriteLine($"This is a bool: {test}, an int: {test1}, a double: {test2}, a char: {test3}, a string: {test4}");
         }
     }
 }
